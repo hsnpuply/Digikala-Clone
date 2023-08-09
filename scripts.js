28dd625ba78fa31aee5cf44726301dfdd765301d
hsnpuply <hsnpuply@gmail.com>
2023-08-09 14:18:30 +0330

    profile panel complete

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const btnAboutUs = document.querySelector('.more-less-content');
 const btnAboutUs_P = document.querySelector('.more-less-content p');
 const contentAboutUs = document.querySelector('.content');
 
+
 let flagAboutUs=false;
 btnAboutUs.addEventListener('click',()=>{
     flagAboutUs==false ? (btnAboutUs_P.textContent='بستن',flagAboutUs=true) :(btnAboutUs_P.textContent='مشاهده بیشتر',flagAboutUs=false);
@@ -9,7 +10,14 @@ btnAboutUs.addEventListener('click',()=>{
 
     contentAboutUs.classList.toggle('more-about-us-reveal');
 
+})
 
 
 
+const userInfo = document.querySelector('.userInfo');
+const profilePanel = document.querySelector('.profile-panel');
+
+userInfo.addEventListener('click',()=>{
+    userInfo.classList.toggle('userInfo-Background')
+    profilePanel.classList.toggle('shown')
 })
