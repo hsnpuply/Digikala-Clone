8581cce211f309602127d17a3f0b5968ca47960f
hsnpuply <hsnpuply@gmail.com>
2023-08-10 11:56:53 +0330

    scroll navbar

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const btnAboutUs = document.querySelector('.more-less-content');
 const btnAboutUs_P = document.querySelector('.more-less-content p');
 const contentAboutUs = document.querySelector('.content');
 
+const navbar_full_line = document.querySelector('.navbar-full-line');
+
 
 let flagAboutUs=false;
 btnAboutUs.addEventListener('click',()=>{
@@ -21,3 +23,33 @@ userInfo.addEventListener('click',()=>{
     userInfo.classList.toggle('userInfo-Background')
     profilePanel.classList.toggle('shown')
 })
+
+
+
+// Navbar scrolling
+
+const axsisY=0;
+
+let lastScrolled = window.scrollY;
+window.addEventListener('scroll',()=>{
+
+    if(window.scrollY >= lastScrolled + 2){
+        // console.log('you are going down buddy');
+        // userInfo.style.backgroundColor='red';
+
+        navbar_full_line.classList.add('navbar-full-line-scroll-down-hide');
+        navbar_full_line.classList.remove('navbar-full-line-scroll-top-shown');
+
+
+    }else if(window.scrollY < lastScrolled - 10){
+        // console.log('to the top');
+        // userInfo.style.backgroundColor='lightblue';
+        navbar_full_line.classList.add('navbar-full-line-scroll-top-shown');
+        navbar_full_line.classList.remove('navbar-full-line-scroll-down-hide');
+
+    }
+     lastScrolled = window.scrollY;
+
+
+});
+
